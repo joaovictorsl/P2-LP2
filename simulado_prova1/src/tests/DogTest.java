e25d750d8e3bac6d1e8460a74be9b08a61cd6289
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2022-06-28 23:59:05 -0300

    Simulado Prova 1

---
+++ simulado_prova1/src/tests/DogTest.java
@@ -0,0 +1,20 @@
+package src.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class DogTest {
+  @Test
+  void testEquals() {
+
+  }
+
+  @Test
+  void testGetQuantRacao() {
+
+  }
+
+  @Test
+  void testToString() {
+
+  }
+}
